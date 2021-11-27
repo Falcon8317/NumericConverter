a59411514edd35ee5f9e26306127e45c21b7d85d
Falcon8317 <77pxfalcon@gmail.com>
2021-11-27 11:18:00 +0100

    First stage of the program.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
 fun main() {
-    
+    print("Enter number in decimal system: ")
+    val convertableNumber = readLine()!!.toInt()
+    print("Enter target base: ")
+    val targetBase = readLine()!!.toInt()
+
+    val hex = Integer.toHexString(convertableNumber)
+    val binary = Integer.toBinaryString(convertableNumber)
+    val octal = Integer.toOctalString(convertableNumber)
+
+    when (targetBase) {
+        2 -> {
+            print("Conversion result: $binary")
+        }
+
+        8 -> {
+            print("Conversion result: $octal")
+        }
+
+        16 -> {
+            print("Conversion result: $hex")
+        }
+    }
 }
