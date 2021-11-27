767f1ded8a0c175b7ca67a3ebd1ca8ad18811a32
Falcon8317 <77pxfalcon@gmail.com>
2021-11-27 13:46:41 +0100

    Extended with convert from and to and multiple converting is allowed now.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,43 @@
 fun main() {
-    print("Enter number in decimal system: ")
-    val convertableNumber = readLine()!!.toInt()
-    print("Enter target base: ")
-    val targetBase = readLine()!!.toInt()
+    do {
+        print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ")
+        val answer = readLine()!!.toString()
+        when (answer) {
+            "/from" -> {
+                print("Enter number in decimal system: ")
+                val convertableNumber = readLine()!!.toInt()
+                print("Enter target base: ")
+                val targetBase = readLine()!!.toInt()
 
-    val hex = Integer.toHexString(convertableNumber)
-    val binary = Integer.toBinaryString(convertableNumber)
-    val octal = Integer.toOctalString(convertableNumber)
+                val hex = Integer.toHexString(convertableNumber)
+                val binary = Integer.toBinaryString(convertableNumber)
+                val octal = Integer.toOctalString(convertableNumber)
 
-    when (targetBase) {
-        2 -> {
-            print("Conversion result: $binary")
+                when (targetBase) {
+                    2 -> {
+                        println("Conversion result: $binary")
+                    }
+
+                    8 -> {
+                        println("Conversion result: $octal")
+                    }
+
+                    16 -> {
+                        println("Conversion result: $hex")
+                    }
+                }
+            }
+            "/to" -> {
+                print("Enter source number: ")
+                val sourceNumber :String = readLine()!!.toString()
+                print("Enter source base: ")
+
+                when (readLine()!!.toInt()) {
+                    16-> println("Conversion to decimal result: ${java.lang.Long.parseLong(sourceNumber,16)}")
+                    8 -> println("Conversion to decimal result: ${java.lang.Long.parseLong(sourceNumber,8)}")
+                    2 -> println("Conversion to decimal result: ${java.lang.Long.parseLong(sourceNumber,2)}")
+                }
+            }
         }
-
-        8 -> {
-            print("Conversion result: $octal")
-        }
-
-        16 -> {
-            print("Conversion result: $hex")
-        }
-    }
+    }while (answer != "/exit")
 }
